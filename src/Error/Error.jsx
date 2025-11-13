7b2f73343434bc86aeaae7ac1eff0e46607fc920
lvfahim <mdfahim26320@gmail.com>
2025-11-13 21:55:58 +0600

    my Error section is okay

--- src/Error/Error.jsx
+++ src/Error/Error.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Img from '../assets/error-404.png'
+import { Link } from 'react-router';
 
 const Error = () => {
     return (
@@ -9,6 +10,9 @@ const Error = () => {
             </div>
             <h1 className='text-center text-5xl font-semibold mt-4'>Oops, page not found!</h1>
             <p className='text-gray-500 text-xl text-center mt-2'>The page you are looking for is not available.</p>
+            <div>
+               <Link to='/'> <h1 className='text-center underline text-3xl text-blue-500 rounded-2xl '>GO Home</h1></Link>
+            </div>
         </div>
     );
 };
